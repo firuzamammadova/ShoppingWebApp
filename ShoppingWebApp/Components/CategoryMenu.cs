04ababd63af9da0eae7d2df292330cc9ca448993
Firuza <firuza.m.mammadova@gmail.com>
2020-04-14 14:37:09 +0400

    done

--- ShoppingWebApp/Components/CategoryMenu.cs
+++ ShoppingWebApp/Components/CategoryMenu.cs
@@ -1,21 +1,31 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using ShoppingWebApp.Services;
+using ShoppingWebApp.ViewModels;
 
 namespace ShoppingWebApp.Components
 {
     public class CategoryMenu : ViewComponent
     {
         private ICategoryService repository;
+        private IUnitOfWork uow;
 
-        public CategoryMenu(ICategoryService _repository)
+        public CategoryMenu(ICategoryService _repository, IUnitOfWork _uow)
         {
             repository = _repository;
+            uow = _uow;
         }
 
         public IViewComponentResult Invoke()
         {
-            return View(repository.GetAllWithProductCount());
+             var list =uow.Categories.GetAll().Select(i => new CategoryModel()
+            {
+                CategoryId = i.Id,
+                CategoryName = i.CategoryName,
+                Count = uow.ProductCategories.GetAll().Where(a=>a.CategoryId==i.Id).Count()
+            });
+            return View(list);
         }
     }
 }
